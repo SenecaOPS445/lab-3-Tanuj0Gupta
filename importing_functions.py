f5dbf99d1e3aa4b283b77f8ee68a61d892daf98e
Tanuj Gupta <tgupta25@myseneca.ca>
2025-01-30 14:14:17 -0500

    importing fuctions

---
+++ importing_functions.py
@@ -0,0 +1,19 @@
+# import lab3a
+# text = lab3a.return_text_value()
+# print(text)
+# print(lab3a.return_number_value())
+
+import lab3b
+
+lab3b.sum_numbers(10, 5)
+# Will return 15
+print(lab3b.sum_numbers(25, 25))
+# Will return 50
+lab3b.subtract_numbers(10, 5)
+# Will return 5
+lab3b.subtract_numbers(5, 10)
+# Will return -5
+lab3b.multiply_numbers(10, 5)
+# Will return 50
+lab3b.multiply_numbers(10, 2)
+# Will return 20
